acc5b3f9706b6636fee50929d5a1790d9b55b027
Carson777 <carsoncapper@gmail.com>
2016-06-23 12:58:43 -0500

    Initial Commit

---
+++ StepView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StepView
+//
+//  Created by Carson Capper on 6/23/16.
+//  Copyright © 2016 Carson Capper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
